ba1c966da148089a6360812af7b422629d9e3689
Sameet Sapra <sameetandpotatoes@gmail.com>
2014-04-02 21:47:54 -0500

    basic styling of github api finished

--- app/controllers/github/sessions_controller.rb
+++ app/controllers/github/sessions_controller.rb
@@ -7,15 +7,10 @@ class Github::SessionsController < ApplicationController
                            :client_secret => ENV["GITHUB_SECRET"],
                            :code => session_code},
                            :accept => :json)
-    # extract the token and granted scopes
     access_token = JSON.parse(result)['access_token']
     scopes = JSON.parse(result)['scope'].split(',')
-    has_user_email_scope = scopes.include? 'user:email'
     @results = JSON.parse(RestClient.get('https://api.github.com/user',
                                         {:params => {:access_token => access_token}}))
-    # if the user authorized it, fetch private emails
-    if has_user_email_scope
-      @results['private_emails'] = JSON.parse(RestClient.get('https://api.github.com/user/emails',{:params => {:access_token => access_token}}))
-    end
+    @results["repos"] = getrequest(@results['repos_url'])
   end
 end
