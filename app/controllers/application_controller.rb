d39148af84f7bbe16c0817fbd2806a7dfa8152bf
Sameet Sapra <sameetandpotatoes@gmail.com>
2014-04-03 17:29:17 -0500

    fixing the database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,11 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound => e
+      return
+    end
   end
   helper_method :current_user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140403214503) do
+ActiveRecord::Schema.define(version: 20140403222627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 20140403214503) do
     t.string "email"
     t.text   "image"
     t.string "e"
-    t.text   "github_results",   default: "--- {}\n"
-    t.text   "linkedin_results", default: "--- {}\n"
+    t.text   "github_results",   default: ""
+    t.text   "linkedin_results", default: ""
   end
 
 end
