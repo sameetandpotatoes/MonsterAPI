09a6c8f5a181e561a3f78e2bed4e96f171d742cc
Sameet Sapra <sameetandpotatoes@gmail.com>
2014-04-03 22:59:00 -0500

    finished basic styling for github api

--- app/controllers/linkedin/sessions_controller.rb
+++ app/controllers/linkedin/sessions_controller.rb
@@ -1,16 +1,17 @@
+# Linkedin Session Controller
 class Linkedin::SessionsController < ApplicationController
   include ApplicationHelper
   def create
-    url = 'https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code=' + params["code"]+'&redirect_uri=' + ENV['URL'] + '/auth/linkedin/callback&client_id='+ENV['LINKED_KEY']+'&client_secret='+ENV['LINKED_SECRET']
+    url = 'https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code=' + params['code'] + '&redirect_uri=' + ENV['URL'] + '/auth/linkedin/callback&client_id=' + ENV['LINKED_KEY'] + '&client_secret=' + ENV['LINKED_SECRET']
     response = postrequest(url)
     @access_token = response["access_token"]
-    url = 'https://api.linkedin.com/v1/people/~?oauth2_access_token='+@access_token+'&format=json'
+    url = 'https://api.linkedin.com/v1/people/~?oauth2_access_token=' + @access_token + '&format=json'
     @basic_info = getrequest(url)
     #Connections
     # url = 'https://api.linkedin.com/v1/people/~/connections?&oauth2_access_token='+@access_token+'&format_json'
     # connections = getrequest(url)
-    if !current_user.nil? && current_user["e"] == "all"
-      current_user["linkedin_results"] = @basic_info
+    if !current_user.nil? && current_user['e'] == 'all'
+      current_user['linkedin_results'] = @basic_info
       current_user.save!
       redirect_to all_path
     end
